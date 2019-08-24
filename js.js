ca704b0ef9cb989bca34c85837a7ac31f1d77bce
Hasim Fadil Alatas <35094236+cakfadil@users.noreply.github.com>
2019-08-25 01:58:33 +0700

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-<script type='text/javascript'>
-//<![CDATA[
-var setting={exceptionurl:"google.com,twitter.com",path:"#?o="};
-//]]>
-</script>
-<script src='https://cdn.jsdelivr.net/gh/Indzign/theme@master/autosafelink.js'/>
-<script src='https://www.blogger.com/feeds/2525300720122578858/posts/default?alt=json-in-script&amp;max-results=5&amp;callback=showurl'/>
