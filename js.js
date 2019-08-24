32c84729a6580547ae9af6ff95f107be27ee1bc6
Hasim Fadil Alatas <35094236+cakfadil@users.noreply.github.com>
2019-08-25 01:45:52 +0700

    Update js.js

--- js.js
+++ js.js
@@ -4,4 +4,4 @@ var setting={exceptionurl:"google.com,twitter.com",path:"#?o="};
 //]]>
 </script>
 <script src='https://cdn.jsdelivr.net/gh/Indzign/theme@master/autosafelink.js'/>
-<script src='https://www.blogger.com/feeds/3441289521812010022/posts/default?alt=json-in-script&amp;max-results=5&amp;callback=showurl'/>
+<script src='https://www.blogger.com/feeds/2525300720122578858/posts/default?alt=json-in-script&amp;max-results=5&amp;callback=showurl'/>
